c427be60e4081f859b77a3ac2ee43b755f289df5
rkyatham01 <rishith.kyatham@stonybrook.edu>
2023-02-02 21:04:36 -0500

    constant types C++

---
+++ constType.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    const double Pi = 3.14; //cannot change a constant in c++
+    //gives variable security for anything labeled constant
+    //as a naming convention, we should capitalize every const 
+    std::cout << Pi << "cm";
+    return 0; //code ran succesfully without any errors
+}
